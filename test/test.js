951b3fadc3b4eef2f95bb373b1c62baf54a9755e
Noah Yarian <noah.yarian@gmail.com>
2015-07-31 10:38:38 -0500

    npm installed testing packages, added test scripts, touched test files

---
+++ test/test.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var should = require('chai').should();
+
+describe('Mocha + Chai', function () {
+  it('truthyness', function () {
+      true.should.be.true;
+      false.should.be.false;
+  });
+});
